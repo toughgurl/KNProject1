b5842e1663a11120daa9139fd15a5a634eb60c12
toughgurl <36879214+toughgurl@users.noreply.github.com>
2018-06-22 09:07:39 +0000

    Add files via upload
    initial upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <bcm2835.h>
+
+// Blinks on RPi pin GPIO 11
+#define PIN RPI_GPIO_P1_11
+
+int main(int argc, char **argv)
+{
+    if (!bcm2835_init())
+	return 1;
+
+    // Set the pin to be an output
+    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+
+    // Blink
+    while (1)
+    {
+	// Turn it on
+	bcm2835_gpio_write(PIN, HIGH);
+	
+	// wait a bit
+	delay(500);
+	
+	// turn it off
+	bcm2835_gpio_write(PIN, LOW);
+	
+	// wait a bit
+	delay(500);
+    }
+    return 0;
+}
